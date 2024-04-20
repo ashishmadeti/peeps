b06cb1cfe0182bc248a40077d4c27918fa403ca4
Ashish Madeti <ashishmadeti@gmail.com>
2024-04-20 11:48:29 +0530

    Add API for updating IP given correct password

--- server/src/app.go
+++ server/src/app.go
@@ -21,13 +21,30 @@ type user struct {
 // TODO: Do not use global variable here
 var db *gorm.DB
 
-
 func getIPAddress(c *gin.Context) string {
 	ip := c.GetHeader("X-Real-IP")
 	if ip == "" {
 		ip = c.ClientIP()
 	}
 	return ip
+}
+
+func getUserByUsername(u string) (user, error) {
+	var userFound user
+
+	if result := db.First(&userFound, user{Username: u}, "Username"); result.Error != nil {
+		return user{}, result.Error
+	}
+	return userFound, nil
+}
+
+func updateIPByUsername(u string, ip string) error {
+	var userFound user
+
+	if result := db.Model(&userFound).Where(user{Username: u}).Updates(user{IP: ip}); result.Error != nil {
+		return result.Error
+	}
+	return nil
 }
 
 // TODO: Add tests for these functions
@@ -37,7 +54,7 @@ func getUsers(c *gin.Context) {
 	if result := db.Find(&users); result.Error != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"message": "something went wrong. please contact administrator.",
-			"error": result.Error.Error(),
+			"error":   result.Error.Error(),
 		})
 		return
 	}
@@ -48,10 +65,10 @@ func createUser(c *gin.Context) {
 	var newUser user
 
 	if err := c.BindJSON(&newUser); err != nil {
-		fmt.Println("Error", err)
+		fmt.Println("Error while adding user", err)
 		c.JSON(http.StatusBadRequest, gin.H{
 			"message": "invalid data",
-			"error": err.Error(),
+			"error":   err.Error(),
 		})
 		return
 	}
@@ -69,7 +86,7 @@ func createUser(c *gin.Context) {
 		}
 		c.JSON(s, gin.H{
 			"message": msg,
-			"error": result.Error.Error(),
+			"error":   result.Error.Error(),
 		})
 		return
 	}
@@ -77,8 +94,51 @@ func createUser(c *gin.Context) {
 	c.JSON(http.StatusCreated, gin.H{"message": "user created"})
 }
 
-// TODO: Allow user to update IP address, provided correct password
-// Use bcrypt.CompareHashAndPassword() for checking if password is correct
+func updateUserIP(c *gin.Context) {
+	var request user
+
+	if err := c.BindJSON(&request); err != nil {
+		fmt.Println("Error while updating IP", err)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "invalid request",
+			"error":   err.Error(),
+		})
+		return
+	}
+
+	foundUser, err := getUserByUsername(request.Username)
+	if err != nil {
+		fmt.Println("Error while finding user", err)
+		msg := "something went wrong. please contact administrator."
+		s := http.StatusInternalServerError
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			msg = "could not find the user"
+			s = http.StatusNotFound
+		}
+		c.JSON(s, gin.H{
+			"message": msg,
+			"error":   err.Error(),
+		})
+		return
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(foundUser.Password), []byte(request.Password)); err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"message": "invalid password",
+		})
+		return
+	}
+
+	if err := updateIPByUsername(request.Username, getIPAddress(c)); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "something went wrong. please contact administrator",
+			"error":   err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusNoContent, gin.H{"message": "IP address updated"})
+}
 
 func main() {
 	// Initialize and connect to sqlite file
@@ -90,12 +150,12 @@ func main() {
 	db.AutoMigrate(&user{})
 
 	router := gin.Default()
-	router.POST("/users", createUser)
 	router.GET("/users", getUsers)
+	router.POST("/users", createUser)
+	router.POST("/users/ip", updateUserIP)
 
 	// This is set according to my home router configuration
 	// TODO: Need to change this when deploying to prod
 	router.SetTrustedProxies([]string{"192.168.0.0/16"})
 	router.Run("0.0.0.0:8080") // This is meant to be run with a reverse proxy like nginx
-
 }
